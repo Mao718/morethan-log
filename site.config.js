4f58b90e13f0f4bd7a9d44dfb08bcf0c4c771667
MAO <60081713+Mao718@users.noreply.github.com>
2023-02-10 13:00:36 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,11 +1,11 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: 'morethanmin',
+    name: 'Mao PoYuan',
     image: '/avatar.svg',  // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: 'frontend developer',
     bio: 'I develop everything using node.',
-    email: 'morethanmin.dev@gmail.com',
+    email: 'mao.kyushu@gmail.com',
     linkedin: 'morethanmin',
     github: 'morethanmin',
     instagram: '',
