77a8931ae47675c9d4b5c456137bcf13b69ec196
MAO <60081713+Mao718@users.noreply.github.com>
2023-02-10 19:09:16 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -12,8 +12,8 @@ const CONFIG = {
   },
   projects: [
     {
-      name: `morethan-log`,
-      href: 'https://github.com/morethanmin/morethan-log'
+      name: `Adverseriral Sample`,
+      href: 'https://github.com/Mao718/attack_zoo'
     }
   ],
   // blog setting (required)
